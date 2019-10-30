f4ea3ac60c278075632ea746121e6609e7a96f9b
Evgenia Tyunkova <tynek@mail.ru>
2019-10-30 18:25:37 +0300

    Протеиновая диета.[#190789]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public int calc(int ivan, int nik) {
+        int month = 0;
+        int ivanStrange = ivan;
+        int nikStrange = nik;
+        while (ivanStrange < nikStrange){
+            month++;
+            ivanStrange*=3;
+            nikStrange*=2;
+        }
+        return month;
+    }
+}
