b27002d99e1bafe3489fb7a295672aa48b6672a4
Evgenia Tyunkova <tynek@mail.ru>
2019-11-02 16:33:29 +0300

    Протеиновая диета.[#190789]-2."
    This reverts commit 3c40527a

--- chapter_001/src/main/java/ru/job4j/loop/Fitness.java
+++ chapter_001/src/main/java/ru/job4j/loop/Fitness.java
@@ -3,10 +3,12 @@ package ru.job4j.loop;
 public class Fitness {
     public int calc(int ivan, int nik) {
         int month = 0;
-        while (ivan < nik){
+        int ivanStrange = ivan;
+        int nikStrange = nik;
+        while (ivanStrange < nikStrange){
             month++;
-            ivan*=3;
-            nik*=2;
+            ivanStrange*=3;
+            nikStrange*=2;
         }
         return month;
     }
