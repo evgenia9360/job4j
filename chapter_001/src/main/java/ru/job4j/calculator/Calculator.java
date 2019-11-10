00ebe04c4bbc59c5b8a6ff66d824edcb3461c719
Evgenia Tyunkova <tynek@mail.ru>
2019-11-10 19:10:26 +0300

    JavaDoc. Code convention[#190787]-2

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -7,21 +7,43 @@ package ru.job4j.calculator;
  */
 public class Calculator {
 
+    /**
+     * Сложение.
+     *
+     * @param first первое число.
+     * @param second второе число.
+     */
     public static void add(int first, int second) {
         int result = first + second;
         System.out.println(first + " + " + second + " = " + result);
     }
 
+    /**
+     * Деление.
+     *
+     * @param first первое число.
+     * @param second второе число.
+     */
     public static void div(int first, int second) {
         int result2 = first / second;
         System.out.println(first + " / " + second + " = " + result2);
     }
 
+    /**
+     * Умножение.
+     * @param first первое число.
+     * @param second второе число.
+     */
     public static void multiply(int first, int second) {
         int result3 = first * second;
         System.out.println(first + " * " + second + " = " + result3);
     }
 
+    /**
+     * Вычитание.
+     * @param first первое число.
+     * @param second второе число.
+     */
     public static void subtract(int first, int second) {
         int result4 = first - second;
         System.out.println(first + " - " + second + " = " + result4);
