bbaf19ebf4f72688773714f255627a5b8ba38776
Evgenia Tyunkova <tynek@mail.ru>
2019-11-20 16:20:46 +0300

    Создание объекта[#190924]

---
+++ chapter_001/src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,15 @@
+package ru.job4j.oop;
+
+/**
+ * Создаем объекты класса Dog.
+ */
+public class Dog {
+    /**
+     * @param args параметры.
+     */
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
