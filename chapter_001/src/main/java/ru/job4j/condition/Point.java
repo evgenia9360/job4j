46fcd3d508d76a98f88e5d542c88c2ed447230b8
Evgenia Tyunkova <tynek@mail.ru>
2019-11-22 16:12:40 +0300

    Рефакторинг - Расстояние между точками.[#190919]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,22 +1,27 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+
 /**
  * Расстояние между точками в системе координат.
  */
 public class Point {
+    private int x;
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
     /**
      * Формула поиска расстояния между точками.
      *
-     * @param x1 точка х1.
-     * @param y1 точка y1.
-     * @param x2 точка х1.
-     * @param y2 точка y2.
      * @return вернуть расстояние между точками.
      */
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        return distance;
 
+    public double distance(Point that) {
+        return sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
     }
 
     /**
@@ -25,7 +30,9 @@ public class Point {
      * @param args параметры.
      */
     public static void main(String[] args) {
-        double result = distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) " + result);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 }
