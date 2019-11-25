44733b8fc1a872ebe74c5a5db621481acb2f2736
Evgenia Tyunkova <tynek@mail.ru>
2019-11-25 14:39:24 +0300

    Перегрузить метод max для трех чисел[#190922]

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -5,11 +5,38 @@ package ru.job4j.condition;
  */
 public class Max {
     /**
+     * Определить максимальное число.
+     *
      * @param first  первое целое число.
-     * @param second второе челое число.
+     * @param second второе целое число.
      * @return вернуть максимальное целое число.
      */
     public static int max(int first, int second) {
-        return first > second ? first : second;
+        return Math.max(first, second);
+    }
+
+    /**
+     * Определить максимальное число.
+     *
+     * @param first  первое целое число.
+     * @param second второе целое число.
+     * @param third  третье целое число.
+     * @return вернуть максимальное целое число.
+     */
+    public static int max(int first, int second, int third) {
+        return Math.max(first, Math.max(second, third));
+    }
+
+    /**
+     * Определить максимальное число.
+     *
+     * @param first  первое целое число.
+     * @param second второе целое число.
+     * @param third  третье целое число.
+     * @param fourth четвертое целое число.
+     * @return вернуть максимальное целое число.
+     */
+    public static int max(int first, int second, int third, int fourth) {
+        return Math.max(first, Math.max(second, Math.max(third, fourth)));
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -19,8 +19,20 @@ public class MaxTest {
     }
 
     @Test
+    public void whenMaxThird() {
+        int result = Max.max(1, 2, 3);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenMaxFourth() {
+        int result = Max.max(1, 2, 3, 4);
+        assertThat(result, is(4));
+    }
+
+    @Test
     public void whenEqually() {
-        int result = Max.max(2, 2);
+        int result = Max.max(2, 2, 2, 2);
         assertThat(result, is(2));
     }
 }
