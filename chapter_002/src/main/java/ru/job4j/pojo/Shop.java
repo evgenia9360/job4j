319b80915a08568c363a55b95267d2323ad5ca3a
Evgenia Tyunkova <tynek@mail.ru>
2019-11-30 14:08:00 +0300

    Удаление моделей из массива.[#190947]

---
+++ chapter_002/src/main/java/ru/job4j/pojo/Shop.java
@@ -0,0 +1,14 @@
+package ru.job4j.pojo;
+
+public class Shop {
+    public static void main(String[] args) {
+
+    }
+
+    public Product[] delete(Product[] products, int index) {
+        products[index] = null;
+        for (int i = index + 1;) {
+
+        }
+    }
+}
